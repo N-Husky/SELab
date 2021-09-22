72928a7ca6ab9a22a43d047e7790aa7e130f00ab
dockg_skun.j_bay@protonmail.com <dockg_skun.j_bay@protonmail.com>
2021-09-22 23:03:46 +0300

    First commint

---
+++ Lab_TPP2_2/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Lab_TPP2_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openAssemblyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Controls.Add(this.menuStrip1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 426);
+            this.splitContainer1.SplitterDistance = 582;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(582, 426);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "1.png");
+            this.imageList1.Images.SetKeyName(1, "2.png");
+            this.imageList1.Images.SetKeyName(2, "3.png");
+            this.imageList1.Images.SetKeyName(3, "4.png");
+            this.imageList1.Images.SetKeyName(4, "5.png");
+            this.imageList1.Images.SetKeyName(5, "6.png");
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(214, 426);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openAssemblyToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openAssemblyToolStripMenuItem
+            // 
+            this.openAssemblyToolStripMenuItem.Name = "openAssemblyToolStripMenuItem";
+            this.openAssemblyToolStripMenuItem.Size = new System.Drawing.Size(100, 20);
+            this.openAssemblyToolStripMenuItem.Text = "Open assembly";
+            this.openAssemblyToolStripMenuItem.Click += new System.EventHandler(this.openAssemblyToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Petrychenko_Bohdan_KI-181_TPP2_2";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openAssemblyToolStripMenuItem;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Lab_TPP2_2/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_TPP2_2
+{
+    public partial class Form1 : Form
+    {
+        private Assembly assembly;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void openAssemblyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+            string path = selectAssemblyFile();
+            if (path != null)
+            {
+                assembly = openAssembly(path);
+            }
+            if (assembly != null)
+            {
+                TreeNode root = new TreeNode();
+                root.Text = assembly.GetName().Name;
+                root.ImageIndex = 0;
+                root.SelectedImageIndex = 0;
+                treeView1.Nodes.Add(root);
+                Type[] types = assembly.GetTypes();
+                addRoot(root, types);
+            }
+        }
+        private string selectAssemblyFile()
+        {
+            openFileDialog1.Filter = "Dll files (*.dll)|*.dll|Exe files (*.exe) | *.exe | All files(*.*) | *.* ";
+            openFileDialog1.Title = "Select an assembly file";
+            return (openFileDialog1.ShowDialog() == DialogResult.OK) ?
+            openFileDialog1.FileName : null;
+        }
+        private Assembly openAssembly(string path)
+        {
+            try
+            {
+                Assembly a = Assembly.LoadFrom(path);
+                return a;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error on loading the assembly!",
+                "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+        void addRoot(TreeNode root, Type[] types)
+        {
+            TreeNode node = null;
+            foreach (Type type in types)
+            {
+                node = new TreeNode();
+                node.Text = type.ToString();
+                if (type.IsClass)
+                {
+                    node.ImageIndex = 1;
+                    node.SelectedImageIndex = 1;
+                    addFirstLevel(node, type);
+                    root.Nodes.Add(node);
+                }
+                else if (type.IsInterface)
+                {
+                    node.ImageIndex = 2;
+                    node.SelectedImageIndex = 2;
+                    addFirstLevel(node, type);
+                    root.Nodes.Add(node);
+                }
+                else if (type.IsEnum)
+                {
+                    node.ImageIndex = 3;
+                    node.SelectedImageIndex = 3;
+                    addFirstLevel(node, type);
+                    root.Nodes.Add(node);
+                }
+            }
+        }
+        private void addFirstLevel(TreeNode node, Type type)
+        {
+            TreeNode node1 = null;
+            FieldInfo[] fields = type.GetFields();
+            FieldInfo[] privatefields = type.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+            MethodInfo[] methods = type.GetMethods();
+            MethodInfo[] privatemethods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+            ConstructorInfo[] constructors = type.GetConstructors();
+            ConstructorInfo[] privateconstructors = type.GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (FieldInfo field in fields)
+            {
+                node1 = new TreeNode();
+                node1.Text = field.FieldType.Name + " " + field.Name;
+                node1.ImageIndex = 5;
+                node1.SelectedImageIndex = 5;
+                node.Nodes.Add(node1);
+            }
+            foreach (FieldInfo field in privatefields)
+            {
+                node1 = new TreeNode();
+                node1.Text = "private " + field.FieldType.Name + " " + field.Name;
+                node1.ImageIndex = 5;
+                node1.SelectedImageIndex = 5;
+                node.Nodes.Add(node1);
+            }
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                string s = "";
+                ParameterInfo[] parametrs = constructor.GetParameters();
+                foreach (ParameterInfo parametr in parametrs)
+                {
+                    s = s + parametr.ParameterType.Name + ", ";
+                }
+                s = s.Trim();
+                s = s.TrimEnd(',');
+                node1 = new TreeNode();
+                node1.Text = node.Text + "(" + s + ")";
+                node1.ImageIndex = 6;
+                node1.SelectedImageIndex = 6;
+                node.Nodes.Add(node1);
+            }
+            foreach (ConstructorInfo constructor in privateconstructors)
+            {
+                string s = "private ";
+                ParameterInfo[] parametrs = constructor.GetParameters();
+                foreach (ParameterInfo parametr in parametrs)
+                {
+                    s = s + parametr.ParameterType.Name + ", ";
+                }
+                s = s.Trim();
+                s = s.TrimEnd(',');
+                node1 = new TreeNode();
+                node1.Text = node.Text + "(" + s + ")";
+                node1.ImageIndex = 6;
+                node1.SelectedImageIndex = 6;
+                node.Nodes.Add(node1);
+            }
+            foreach (MethodInfo method in methods)
+            {
+                string s = "";
+                ParameterInfo[] parametrs = method.GetParameters();
+                foreach (ParameterInfo parametr in parametrs)
+                {
+                    s = s + parametr.ParameterType.Name + ", ";
+                }
+                s = s.Trim();
+                s = s.TrimEnd(',');
+                node1 = new TreeNode();
+                node1.Text = method.ReturnType.Name + " " + method.Name + "(" + s + ")";
+                node1.ImageIndex = 4;
+                node1.SelectedImageIndex = 4;
+                node.Nodes.Add(node1);
+            }
+            foreach (MethodInfo method in privatemethods)
+            {
+                string s = "";
+                ParameterInfo[] parametrs = method.GetParameters();
+                foreach (ParameterInfo parametr in parametrs)
+                {
+                    s = s + "private " + parametr.ParameterType.Name + ", ";
+                }
+                s = s.Trim();
+                s = s.TrimEnd(',');
+                node1 = new TreeNode();
+                node1.Text = method.ReturnType.Name + " " + method.Name + "(" + s + ")";
+                node1.ImageIndex = 4;
+                node1.SelectedImageIndex = 4;
+                node.Nodes.Add(node1);
+            }
+        }
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeNode node = treeView1.SelectedNode;
+            listView1.Clear();
+
+            switch (treeView1.SelectedNode.Level)
+            {
+                case 0:
+                    listView1.Items.Add(" Assembly\n");
+                    listView1.Items.Add(" Name - " + assembly.GetName().Name);
+                    break;
+                case 1:
+                    foreach (var type in assembly.GetTypes())
+                    {
+                        if (type.ToString() == node.Text)
+                        {
+                            if (type.IsClass)
+                            {
+                                Type[] itfcs = type.GetInterfaces();
+                                listView1.Items.Add("Class");
+                                listView1.Items.Add(" Name: " + type.FullName);
+                                string modificator = type.IsNotPublic ? "private" :
+                                                     type.IsPublic ? "public" : "undefined";
+                                listView1.Items.Add(" Access: " + modificator);
+                                listView1.Items.Add(" Is Abstract: " + type.IsAbstract);
+                                listView1.Items.Add(" Is Sealed: " + type.IsSealed);
+                                listView1.Items.Add(" Inherited from: " + type.BaseType.Name);
+                                listView1.Items.Add(" Implements interfaces: ");
+                                foreach (var itfc in itfcs)
+                                    listView1.Items.Add("    " + itfc.Name);
+                            }
+                            else if (type.IsInterface)
+                            {
+                                listView1.Items.Add("Interface");
+                                listView1.Items.Add(" Name: " + type.FullName);
+                                string modificator = type.IsNotPublic ? "private" :
+                                                     type.IsPublic ? "public" : "undefined";
+                                listView1.Items.Add(" Access: " + modificator);
+                            }
+                            else if (type.IsEnum)
+                            {
+                                listView1.Items.Add("Enumeration");
+                                listView1.Items.Add(" Name: " + type.FullName);
+                                string modificator = type.IsNotPublic ? "private" :
+                                                     type.IsPublic ? "public" : "undefined";
+                                listView1.Items.Add(" Access: " + modificator);
+                            }
+                        }
+                    }
+                    break;
+                case 2:
+                    foreach (var type in assembly.GetTypes())
+                    {
+                        if (type.ToString() == node.Parent.Text)
+                        {
+                            var fields = type.GetRuntimeFields();
+                            MethodInfo[] methods = type.GetMethods();
+                            ConstructorInfo[] constructors = type.GetConstructors();
+                            string elemName = node.Text.Split('(').First().Split(' ').Last();
+
+                            foreach (var field in fields)
+                            {
+                                if (field.Name == elemName)
+                                {
+                                    listView1.Items.Add("Field characteristics");
+                                    listView1.Items.Add(" Name: " + field.Name);
+                                    string modificator = field.IsPrivate ? "private" :
+                                                         field.IsPublic ? "public" : "undefined";
+                                    listView1.Items.Add(" Access: " + modificator);
+
+                                    listView1.Items.Add("Primitive");
+                                }
+                            }
+                            foreach (var constructor in constructors)
+                            {
+                                if (constructor.Name == elemName)
+                                {
+                                    listView1.Items.Add("Constructor characteristics");
+                                    listView1.Items.Add(" Name: " + constructor.Name);
+                                    string modificator = constructor.IsPrivate ? "private" :
+                                                         constructor.IsPublic ? "public" : "undefined";
+                                    listView1.Items.Add(" Access: " + modificator);
+
+                                    listView1.Items.Add(" Constructor parametrs: ");
+                                    foreach (var param in constructor.GetParameters())
+                                        listView1.Items.Add("  " + param.Name);
+                                }
+                            }
+                            foreach (var method in methods)
+                            {
+                                if (method.Name == elemName)
+                                {
+                                    listView1.Items.Add("Method characteristics");
+                                    listView1.Items.Add(" Type: Method");
+                                    listView1.Items.Add(" Name: " + method.Name);
+                                    string modificator = method.IsPrivate ? "private" :
+                                                         method.IsPublic ? "public" : "undefined";
+                                    listView1.Items.Add(" Access: " + modificator);
+                                    listView1.Items.Add(" Method parametrs: ");
+                                    foreach (var param in method.GetParameters())
+                                        listView1.Items.Add("  " + param.Name);
+                                }
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
